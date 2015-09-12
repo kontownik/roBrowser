7009389352a6d1f59a88927349fcb023f834b1fe
asdf <tehasdf@gmail.com>
2015-09-12 20:29:06 +0200

    EffectManager: remove by AID before adding to work with moving songs

--- src/Renderer/EffectManager.js
+++ src/Renderer/EffectManager.js
@@ -72,6 +72,7 @@ define(function( require )
 	EffectManager.add = function add(effect, uid, persistent)
 	{
 		var name = (effect.constructor._uid || (effect.constructor._uid = (_uniqueId++)));
+		this.remove({name: name}, uid);
 
 		if (!(name in _list)) {
 			_list[name] = [];
@@ -88,6 +89,7 @@ define(function( require )
 		if (effect.init) {
 			effect.init(_gl);
 		}
+
 
 		effect._uid        = uid;
 		effect._persistent = !!persistent;
