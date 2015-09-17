7e29646c609d5028f1edeaf43d0887b204ebbf10
asdf <tehasdf@gmail.com>
2015-09-17 14:42:37 +0200

    blah blah spirit sphere color

--- src/Renderer/Effects/SpiritSphere.js
+++ src/Renderer/Effects/SpiritSphere.js
@@ -85,10 +85,10 @@ define(function( require ) {
         'void main(void) {',
             'vec4 texture = texture2D( uDiffuse,  vTextureCoord.st );',
 
-            'if (texture.r < 0.5 || texture.g < 0.5 || texture.b < 0.5) {',
+            'if (texture.r < 0.3 || texture.g < 0.3 || texture.b < 0.3) {',
             '   discard;',
             '}',
-            'texture.a = 0.9;',
+            'texture.a = 1.0;',
             'gl_FragColor = texture;',
 
         '}'
